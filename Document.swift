c93079968acb3c7834ebff477dd99e9bd62b1faf
Lebedd <153133856+Lebedd@users.noreply.github.com>
2024-11-29 16:01:30 +0200

    Add files via upload

---
+++ Document.swift
@@ -0,0 +1,27 @@
+//
+//  Document.swift
+//  Gith
+//
+//  Created on 28.11.2024.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+    /*code 1
+    
+    
+    code....
+    */
+  
+}
+
